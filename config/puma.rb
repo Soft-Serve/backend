96b8c821be43bac7b8a8e904d5e547c0f1f5fac6
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-11-09 22:25:15 +0100

    fix config

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3091 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 on_worker_boot do
