60998d4d9a4a9aa5b72d4ba4145a1f812f2ee4f9
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-11-13 12:18:13 -0500

    Use env vars for email and password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,18 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV['EMAIL']
+    :password           => ENV['PASSWORD']
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
