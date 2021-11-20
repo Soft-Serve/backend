38e24277f0a3b6008421073d4feb1c0f3d8b7676
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-11-20 18:24:16 -0500

    checklist

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,7 @@ class Menu < ApplicationRecord
   # Relations
   belongs_to :restaurant
   has_many :menu_categories
+  has_many :menu_items, through: :menu_categories
 
   # Validations
   validates_presence_of :name, :restaurant_id
