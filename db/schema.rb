1d869e85756bd8963903a9f31fe74d6283290c77
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-12-15 18:55:56 -0500

    add fonts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_230757) do
+ActiveRecord::Schema.define(version: 2021_12_15_234928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_230757) do
     t.string "postal_code"
     t.string "country"
     t.boolean "onboarding_done", default: false
+    t.string "font", null: false
   end
 
   create_table "users", force: :cascade do |t|
