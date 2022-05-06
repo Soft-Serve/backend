7110ee5e97cca7e5b94adf50fce83a8b4bc88243
Kristine McBride <kristinelmcbride@gmail.com>
2022-05-06 13:33:12 -0400

    undo stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'restaurants#index'
 
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: "registrations", passwords: "passwords", sessions: "sessions" }
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: "registrations", sessions: "sessions" }
 
   resources :restaurants, only: %i[show create update destroy] do
     resources :dietaries, only: %i[index]
