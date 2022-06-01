f0627aa3d849d50f5d16c7873ddee90ccc644b32
Kristine McBride <kristinelmcbride@gmail.com>
2022-05-31 23:26:24 -0400

    fix promo price

--- app/presenters/menu_category_presenter.rb
+++ app/presenters/menu_category_presenter.rb
@@ -10,7 +10,8 @@ class MenuCategoryPresenter
       id: item.id,
       name: item.name,
       category_type: item.category_type,
-      menu_id: item.menu_id
+      menu_id: item.menu_id,
+      has_active_promo: item.promotion_categories.select{ |promo_cat| promo_cat.promotion.active? }.any?
     }
   end
 end
